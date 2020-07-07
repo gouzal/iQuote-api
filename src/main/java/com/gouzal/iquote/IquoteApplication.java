81c851da9481905efabcac428b220811fb2d5d66
gouzal <larbi-inco@live.fr>
2020-07-07 21:38:10 +0100

    initial init

---
+++ src/main/java/com/gouzal/iquote/IquoteApplication.java
@@ -0,0 +1,13 @@
+package com.gouzal.iquote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IquoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IquoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gouzal/iquote/IquoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gouzal.iquote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IquoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
