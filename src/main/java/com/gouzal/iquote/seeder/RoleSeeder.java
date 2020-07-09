008266c36f5bb7572d2a0baceb118a073c3304c3
gouzal <larbi-inco@live.fr>
2020-07-09 21:15:49 +0100

    add Role Seeder

--- src/main/java/com/gouzal/iquote/seeder/RoleSeeder.java
+++ src/main/java/com/gouzal/iquote/seeder/RoleSeeder.java
@@ -1,11 +1,26 @@
 package com.gouzal.iquote.seeder;
 
+import com.gouzal.iquote.model.Role;
+import com.gouzal.iquote.service.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RoleSeeder extends AbstractTableSeeder {
+    final RoleService roleService;
+
+    @Autowired
+    public RoleSeeder(RoleService roleService) {
+        this.roleService = roleService;
+    }
+
     @Override
     public void run() {
-
+        String[] roles = {"normal_user", "admin"};
+        for (int i = 0; i < roles.length; i++) {
+            Role role = new Role();
+            role.setLabel(roles[i]);
+            this.roleService.save(role);
+        }
     }
 }
