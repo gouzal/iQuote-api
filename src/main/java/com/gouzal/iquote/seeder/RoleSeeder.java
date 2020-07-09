0e8ef40f3cea6a606c48116e9373e0d6759e8974
gouzal <larbi-inco@live.fr>
2020-07-09 19:56:35 +0100

    add RoleSeeder

---
+++ src/main/java/com/gouzal/iquote/seeder/RoleSeeder.java
@@ -0,0 +1,11 @@
+package com.gouzal.iquote.seeder;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RoleSeeder extends AbstractTableSeeder {
+    @Override
+    public void run() {
+
+    }
+}
