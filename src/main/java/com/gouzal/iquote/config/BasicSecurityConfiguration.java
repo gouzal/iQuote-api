8dfa9ed7be315917ea3c000f6b76a78866bbe538
gouzal <larbi-inco@live.fr>
2020-07-10 19:29:09 +0100

    add a basic security config

---
+++ src/main/java/com/gouzal/iquote/config/BasicSecurityConfiguration.java
@@ -0,0 +1,28 @@
+package com.gouzal.iquote.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@EnableWebSecurity
+@Component
+public class BasicSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/**").permitAll();
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        // PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+
+}
