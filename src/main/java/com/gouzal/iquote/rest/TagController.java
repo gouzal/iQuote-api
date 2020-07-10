aad341f715a9bc3759274011a5db5eb9cf168857
gouzal <larbi-inco@live.fr>
2020-07-10 20:22:27 +0100

    Add Tag Api

--- src/main/java/com/gouzal/iquote/rest/TagController.java
+++ src/main/java/com/gouzal/iquote/rest/TagController.java
@@ -1,15 +1,31 @@
 package com.gouzal.iquote.rest;
 
+import com.gouzal.iquote.model.Tag;
+import com.gouzal.iquote.service.TagService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @Slf4j
 public class TagController {
+    @Autowired
+    TagService tagService;
 
-    @GetMapping(value = "/index")
-    public String index() {
-        return "hello world";
+    @GetMapping(value = "/api/tags")
+    public ResponseEntity<List> list() {
+        return new ResponseEntity<List>(tagService.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/api/tags/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Tag> byId(@PathVariable long id) {
+        return new ResponseEntity<Tag>(tagService.findById(id), HttpStatus.OK);
     }
 }
