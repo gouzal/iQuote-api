55f88aeef0711fb267bc355a7ddbdd63f4d0b86c
gouzal <larbi-inco@live.fr>
2020-07-10 20:33:23 +0100

    add Author API

---
+++ src/main/java/com/gouzal/iquote/rest/AuthorController.java
@@ -0,0 +1,34 @@
+package com.gouzal.iquote.rest;
+
+import com.gouzal.iquote.model.Author;
+import com.gouzal.iquote.service.AuthorService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@Slf4j
+public class AuthorController {
+    final
+    AuthorService authorService;
+
+    public AuthorController(AuthorService authorService) {
+        this.authorService = authorService;
+    }
+
+    @GetMapping(value = "/api/quotes")
+    public ResponseEntity<List<Author>> list() {
+        return new ResponseEntity<>(authorService.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/api/quotes/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Author> byId(@PathVariable long id) {
+        return new ResponseEntity<>(authorService.findById(id), HttpStatus.OK);
+    }
+}
