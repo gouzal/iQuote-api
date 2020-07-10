64d269ab1edb3b43dc570f202123e31ae28d762a
gouzal <larbi-inco@live.fr>
2020-07-10 20:36:26 +0100

    Add User Api

---
+++ src/main/java/com/gouzal/iquote/rest/UserController.java
@@ -0,0 +1,34 @@
+package com.gouzal.iquote.rest;
+
+import com.gouzal.iquote.model.User;
+import com.gouzal.iquote.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@Slf4j
+public class UserController {
+    final
+    UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping(value = "/api/users")
+    public ResponseEntity<List<User>> list() {
+        return new ResponseEntity<>(userService.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/api/users/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<User> byId(@PathVariable long id) {
+        return new ResponseEntity<>(userService.findById(id), HttpStatus.OK);
+    }
+}
