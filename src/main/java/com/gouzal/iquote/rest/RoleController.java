d4ce0e3f278894c886ef789f65be971835d85806
gouzal <larbi-inco@live.fr>
2020-07-10 20:35:19 +0100

    Add Role Api

---
+++ src/main/java/com/gouzal/iquote/rest/RoleController.java
@@ -0,0 +1,34 @@
+package com.gouzal.iquote.rest;
+
+import com.gouzal.iquote.model.Role;
+import com.gouzal.iquote.service.RoleService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@Slf4j
+public class RoleController {
+    final
+    RoleService roleService;
+
+    public RoleController(RoleService roleService) {
+        this.roleService = roleService;
+    }
+
+    @GetMapping(value = "/api/tags")
+    public ResponseEntity<List<Role>> list() {
+        return new ResponseEntity<>(roleService.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/api/tags/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Role> byId(@PathVariable long id) {
+        return new ResponseEntity<>(roleService.findById(id), HttpStatus.OK);
+    }
+}
