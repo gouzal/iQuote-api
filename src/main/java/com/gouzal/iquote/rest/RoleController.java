8155cc36dc56911cf5830bebbdb1104a8dd30d96
gouzal <larbi-inco@live.fr>
2020-07-26 15:54:34 +0100

    add CrossOrigin

--- src/main/java/com/gouzal/iquote/rest/RoleController.java
+++ src/main/java/com/gouzal/iquote/rest/RoleController.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import java.util.List;
 
 @RestController
 @Slf4j
+@CrossOrigin
 public class RoleController {
     final
     RoleService roleService;
